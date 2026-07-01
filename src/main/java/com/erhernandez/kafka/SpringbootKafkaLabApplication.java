8d32ee93fb8fe194aa71030d21fb028662f98f72
Edgar Ricardo Hernandez Fonseca <erhernandez2000@yahoo.com>
2026-07-01 11:15:13 -0500

    EH | v1.0.0 - Initial Spring Boot Kafka project setup. | Ok

---
+++ src/main/java/com/erhernandez/kafka/SpringbootKafkaLabApplication.java
@@ -0,0 +1,13 @@
+package com.erhernandez.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erhernandez/kafka/SpringbootKafkaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erhernandez.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
